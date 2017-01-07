bf2a8ab1f5ecf91ee0da3539441c8d0b0ef40832
calvarado <calvarado@wesleyan.edu>
2017-01-07 18:58:00 -0500

    Cleaned helper functions and changed the output taype of the main function.

--- removingPOS/verb_remove.py
+++ removingPOS/verb_remove.py
@@ -7,13 +7,11 @@ def verb_removal(st):
         for i in range(len(lst)):
             if lst[i][2] == "VERB":
                 verb_tup_lst = [lst[i]]
-                indexes_mached = [i]
                 for p in range(i+1,len(lst)):
                     if lst[p][2] == "VERB":
                         verb_tup_lst.append(lst[p])
-                        indexes_mached.append(p)
                     else:
-                        return (verb_tup_lst,indexes_mached)
+                        return verb_tup_lst
         return []
 
     #returns a string with all the hypenated words removed
@@ -34,27 +32,26 @@ def verb_removal(st):
             st = delete_words_string(st,word)
         return st
 
+    #removes verbs from sentences wuth hypens
     def hypen_verb_removal(s):
-        tup_sent_arr = pos_tup_list(s)
-        st_wo_hypen_words = remove_hypens_words(s)
-        tup_upd_st = pos_tup_list(st_wo_hypen_words)
-        verb_list_found = consec_verb_list(tup_upd_st)
+        complete_pos_list = pos_tup_list(s)
+        wo_hypen_pos_list = pos_tup_list(remove_hypens_words(s))
+        verb_list_found = consec_verb_list(wo_hypen_pos_list)
         if verb_list_found != []:
-            verb = make_str(verb_list_found[0])
-            sentence_wo_verb = delete_words_string(s,verb)
-            return (sentence_wo_verb[0].upper()+sentence_wo_verb[1:],verb,tup_list_to_string(verb_list_found[0]),tup_list_to_string(tup_sent_arr))
+            updated_pos_list = remove_POS_matches(complete_pos_list, verb_list_found, 'verb')
+            return (complete_pos_list, updated_pos_list, verb_list_found)
         else:
-            return ("ERROR",tup_list_to_string(tup_sent_arr))
+            return ("ERROR",complete_pos_list)
 
+        #normal verb removal:
     def normal_verb_removal(s):
-        tup_sent_arr = pos_tup_list(s)
-        verb_list_found = consec_verb_list(tup_sent_arr)
+        complete_pos_list = pos_tup_list(s)
+        verb_list_found = consec_verb_list(complete_pos_list)
         if verb_list_found != []:
-            verb = make_str(verb_list_found[0])
-            sentence_wo_verb = delete_words_string(s,verb)
-            return (sentence_wo_verb[0].upper()+sentence_wo_verb[1:],verb,tup_list_to_string(verb_list_found[0]),tup_list_to_string(tup_sent_arr))
+            updated_pos_list = remove_POS_matches(complete_pos_list, verb_list_found, 'verb')
+            return (complete_pos_list, updated_pos_list, verb_list_found)
         else:
-            return ("ERROR",tup_list_to_string(tup_sent_arr))
+            return ("ERROR",complete_pos_list)
 
     if hypen_in_sentence(st) == False:
         return normal_verb_removal(st)
