a153f0463fc6606e9060fa110d455afe43bc3ef0
calvarado <calvarado@wesleyan.edu>
2017-01-08 14:50:54 -0500

    I reorganized my code to follow the style guide for python code in this link: https://www.python.org/dev/peps/pep-0008/

--- removingPOS/noun_verb_remove.py
+++ removingPOS/noun_verb_remove.py
@@ -1,14 +1,27 @@
+'''
+Comments go in here:
+'''
+
+
 from nlpSpacy import *
 from noun_remove import noun_removal
 from verb_remove import verb_removal
 
 def noun_verb_removal(st):
-    complete_pos_list = pos_tup_list(st)
-    remove_noun_st = noun_removal(st)
-    remove_verb_st = verb_removal(st)
-    if remove_noun_st[0] != "ERROR" and remove_verb_st[0] != "ERROR":
-        updated_pos_list_noun = remove_POS_matches(complete_pos_list,remove_noun_st[2][1],'noun')
-        updated_pos_list_verb =  remove_POS_matches(updated_pos_list_noun,remove_verb_st[2],'verb')
-        return (complete_pos_list, updated_pos_list_verb, (remove_noun_st[2][0], remove_verb_st[2]))
+    sentence_pos_list = pos_tup_list(st)
+    st_wo_noun = noun_removal(st)
+    st_wo_verb = verb_removal(st)
+    if (st_wo_noun[0] != "ERROR" and
+        st_wo_verb[0] != "ERROR"):
+        upd_pos_list_noun = remove_POS_matches(sentence_pos_list,
+                                               st_wo_noun[2][1],
+                                               'noun')
+        upd_pos_list_verb =  remove_POS_matches(upd_pos_list_noun,
+                                                st_wo_verb[2],
+                                                'verb')
+        return (sentence_pos_list,
+                upd_pos_list_verb,
+                (st_wo_noun[2][0],
+                 st_wo_verb[2]))
     else:
-        return ("ERROR", complete_pos_list)
+        return ("ERROR", sentence_pos_list)
