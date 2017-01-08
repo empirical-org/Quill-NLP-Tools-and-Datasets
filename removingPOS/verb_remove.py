447a1ab0a19857cede4eec8212f618371ce5ea4e
calvarado <calvarado@wesleyan.edu>
2017-01-08 14:50:26 -0500

    I reorganized my code to follow the style guide for python code in this link: https://www.python.org/dev/peps/pep-0008/

--- removingPOS/verb_remove.py
+++ removingPOS/verb_remove.py
@@ -1,59 +1,67 @@
+'''                                                                    |
+removes verbs from a sentence
+'''
+
 from nlpSpacy import *
 
-#removes verbs from a sentence
 def verb_removal(st):
-    #returns a list of words that are consecutive verbs
-    def consec_verb_list(lst):
+    def verb_list_found(lst):
+        #returns a list of words that are consecutive verbs
         for i in range(len(lst)):
-            if lst[i][2] == "VERB":
+            if (lst[i][2] == "VERB"):
                 verb_tup_lst = [lst[i]]
                 for p in range(i+1,len(lst)):
-                    if lst[p][2] == "VERB":
+                    if (lst[p][2] == "VERB"):
                         verb_tup_lst.append(lst[p])
                     else:
                         return verb_tup_lst
         return []
 
-    #returns a string with all the hypenated words removed
     def remove_hypens_words(st):
-        #removes word duplicates in a list
+        #returns a string with all the hypenated words removed
         def remove_duplicates(lst):
+            #removes word duplicates in a list
             word_dict = {}
             for i in range(len(lst)):
                 word = lst[i]
-                if word_dict.get(word) == None:
+                if (word_dict.get(word) == None):
                     word_dict[word] = word
             return list(word_dict.keys())
 
-        regex = re.compile(r'\w+\-\w+\-\w+|\w+.\-.\w+.\-.\w+|\w+\-\w+|\w+.\-.\w+')
-        matches_lst = remove_duplicates(regex.findall(st)) #this is essential
+        reg = re.compile(r'\w+\-\w+\-\w+|\w+.\-.\w+.\-.\w+|\w+\-\w+|\w+.\-.\w+')
+        matches_lst = remove_duplicates(reg.findall(st))
         while matches_lst != []:
             word = matches_lst.pop()
-            st = delete_words_string(st,word)
+            st = delete_words_string(st, word)
         return st
 
-    #removes verbs from sentences wuth hypens
     def hypen_verb_removal(s):
-        complete_pos_list = pos_tup_list(s)
+        #removes verbs from sentences wuth hypens
+        sentence_pos_list = pos_tup_list(s)
         wo_hypen_pos_list = pos_tup_list(remove_hypens_words(s))
-        verb_list_found = consec_verb_list(wo_hypen_pos_list)
-        if verb_list_found != []:
-            updated_pos_list = remove_POS_matches(complete_pos_list, verb_list_found, 'verb')
-            return (complete_pos_list, updated_pos_list, verb_list_found)
+        verb_list = verb_list_found(wo_hypen_pos_list)
+        if (verb_list != []):
+            upd_pos_list = remove_POS_matches(sentence_pos_list,
+                                              verb_list,
+                                              'verb')
+            return (sentence_pos_list, upd_pos_list, verb_list)
         else:
-            return ("ERROR",complete_pos_list)
+            return ("ERROR", sentence_pos_list)
 
-        #normal verb removal:
+
     def normal_verb_removal(s):
-        complete_pos_list = pos_tup_list(s)
-        verb_list_found = consec_verb_list(complete_pos_list)
-        if verb_list_found != []:
-            updated_pos_list = remove_POS_matches(complete_pos_list, verb_list_found, 'verb')
-            return (complete_pos_list, updated_pos_list, verb_list_found)
+        #normal verb removal:
+        sentence_pos_list = pos_tup_list(s)
+        verb_list = verb_list_found(sentence_pos_list)
+        if (verb_list != []):
+            upd_pos_list = remove_POS_matches(sentence_pos_list,
+                                              verb_list,
+                                              'verb')
+            return (sentence_pos_list, upd_pos_list, verb_list)
         else:
-            return ("ERROR",complete_pos_list)
+            return ("ERROR", sentence_pos_list)
 
-    if hypen_in_sentence(st) == False:
+    if (hypen_in_sentence(st) == False):
         return normal_verb_removal(st)
     else:
         return hypen_verb_removal(st)
