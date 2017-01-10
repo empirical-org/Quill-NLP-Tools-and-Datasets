57ba741a27a2cc3a589a74c51b18d54cee96881f
calvarado <calvarado@wesleyan.edu>
2017-01-10 02:06:06 -0500

    Added comments describing the functions in the file. I also added another function that processes sentences that contain subordinate conjunctions.

--- removingPOS/writing_text_files.py
+++ removingPOS/writing_text_files.py
@@ -1,17 +1,77 @@
 '''
-Create a new branch
-CODE IS GOING TO BE CHANGED NOW: I want everything to output the following -
-For each newly created file start creating final text files:
-First file output: original list of POS || updated list POS || word removed
-Second file output to help people understand: Original sentence: , Word removed: , Updated sentence: , POS original,POS removed words, POS updated
+Most of the functions in this file write information to other files. The
+functions make use of noun_remove.py, verb_remove.py, noun_verb_remove.py,
+and ____________ to remove nouns, verbs, or subordinate conjunctions from
+sentences. Each function reads textfile data from the specified file
+located in the folder called orginalSentences and processes each line. I
+will explain each function in detail below:
 
+make_upd_sentence(s,match_found,removal_type):
+This function takes in a string and a list of tuples. It updates the
+string by removing any words that are in the list of tuples. For
+example, if thestring is "My family loves hiking." and the list of
+tuples is [('family', 'nsubj', 'NOUN'), ('went', 'ROOT', 'VERB')] then
+it returns "My hiking."
+Args:
+    s: A string that is a sentence.
+    match_found: An lst of words to remove. Check the comments on
+                 nlpSpacy.py to see what an lst is.
+    removal_type: This is the kind of removal that needs to be made and
+                  represents the pos of words found in match_found.
+                  Possible words are "noun", and "verb".
+Return:
+    This returns an updated string with the same words as s, but with
+    words in match found removed.
+-----------------------------------------------------------------------
+* Refer to nlpSpacy.py to see what lst is. lst will be used here.
+The functions below handle writing information to two seperate files.
+Information is writtten to a textfile and another file ending in .p,
+which uses pickle. The textfile is for a person to understand how each
+line is processed. Each line in the textfile is in the following format:
+ Original sentence: | Word removed: | Updated sentence: |
+ POS lst for sentence | POS lst of removed words| POS lit of updated sentence
+The information written into the file ending in .p uses serialization.
+Here I write tuples, with three inputs, directly to the file. Each tuple
+contains information for a sentence that was processed. The first argument
+is the pos lst of the line being processed. The second input is a pos lst
+of the original sentence, but does not contain the speficied pos being
+removed. The third input is a lst of the words that were removed. I use
+serialization here so that others could easily reuse the final outputs
+of the data.
+-----------------------------------------------------------------------
+noun_textfile():
+This function reads from a texfile called nounScreening.txt in the folder
+originalSentences. It processes each line and removes the first noun or
+group of consecutive nouns found in the line. Each line is a complete
+sentence. Once a sentence is processed, it writes information to two
+seperate files.
+Args: None
+Return: Nothing is returned, instead nformation is written to other files.
+
+verb_textfile():
+This function reads from a texfile called verbScreening.txt in the folder
+originalSentences. It processes each line and removes the first verb or
+group of consecutive verbs found in the line. Each line is a complete
+sentence. Once a sentence is processed, it writes information to two
+seperate files.
+Args: None
+Return: Nothing is returned, instead nformation is written to other files.
+
+noun_verb_textfile():
+This function reads from a texfile called nounverbScreening.txt in the folder
+originalSentences. It processes each line and removes the first noun or
+group of consecutive nouns and the first verb or group of consecutive verbs
+found in the line. Each line is a complete sentence. Once a sentence is
+processed, it writes information to the two seperate files.
+Args: None
+Return: Nothing is returned, instead nformation is written to other files.
 '''
-
 import pickle
 from nlpSpacy import *
 from noun_remove import noun_removal
 from verb_remove import verb_removal
 from noun_verb_remove import noun_verb_removal
+from conjunction_remove import subordinate_removal
 
 def load(filename):
     with open(filename, "rb") as f:
@@ -31,70 +91,112 @@ def load(filename):
 #     print(tup)
 
 
-def make_updated_sentence(s,match_found,removal_type):
+def make_upd_sentence(s,match_found,removal_type):
     sentence_wo_noun = delete_words_string(s,match_found)
     if (removal_type == "noun_verb"):
         return sentence_wo_noun
     else:
         return sentence_wo_noun[0].upper() + sentence_wo_noun[1:]
 
-#NOUN:
 def noun_textfile():
-    with open('./updatedSentences/nounSentences/errorSentences.txt','w') as error_n, \
-         open('./updatedSentences/nounSentences/detailedRemoval.txt','w') as details_removal, \
-         open('./updatedSentences/nounSentences/nounRemoval.p','wb') as tuple_write, \
-         open('./originalSentences/testingFile.txt','r') as f:
+    noun_dir = "./updatedSentences/nounSentences/"
+    with open(noun_dir + "errorSentences.txt","w") as err, \
+         open(noun_dir + "./updatedSentences/nounSentences/detailedRemoval.txt","w") as det, \
+         open(noun_dir + "./updatedSentences/nounSentences/nounRemoval.p","wb") as tup_write, \
+         open("./originalSentences/testingFile.txt","r") as f:
             for line in f:
-                line = line.rstrip('\n')
-                n_sentence_rem = noun_removal(line)
-                if (n_sentence_rem[0] != "ERROR"):
-                    noun_pos = n_sentence_rem[2][0]
-                    pickle.dump((n_sentence_rem[0], #serialization
-                                 n_sentence_rem[1],
+                line = line.rstrip("\n")
+                line_wo_n = noun_removal(line)
+                if (line_wo_n[0] != "ERROR"):
+                    noun_pos = line_wo_n[2][0]
+                    pickle.dump((line_wo_n[0], #serialization
+                                 line_wo_n[1],
                                  noun_pos),
-                                tuple_write)
-                    noun_found = make_str(noun_pos).rstrip('\n')
-                    details_removal.write("Original Sentence: " + line + " ||| Word Removed: " + noun_found + " ||| Updated Sentence: " + make_updated_sentence(line,noun_found,"").rstrip('\n') + " ||| POS Original Sentence: " +  tup_list_to_string(n_sentence_rem[0]) + " ||| POS Word Removed: " + tup_list_to_string(noun_pos) + " ||| POS Updated Sentence: " + tup_list_to_string(n_sentence_rem[1]) + '\n')
+                                tup_write)
+                    n_found = make_str(noun_pos).rstrip("\n")
+                    det.write(line + " ||| "
+                              + n_found + " ||| "
+                              + make_upd_sentence(line,n_found,"").rstrip("\n") + " ||| "
+                              + tup_list_to_string(line_wo_n[0]) + " ||| "
+                              + tup_list_to_string(noun_pos) + " ||| "
+                              + tup_list_to_string(line_wo_n[1]) + "\n")
                 else:
-                    error_n.write(line + ' ||| POS Original Sentence: ' + tup_list_to_string(n_sentence_rem[1]) + '\n')
-# #verb
+                    err.write(line + ' ||| '
+                              + tup_list_to_string(line_wo_n[1]) + "\n")
+
 def verb_textfile():
-    with open('./updatedSentences/verbSentences/errorSentences.txt','w') as error_v, \
-         open('./updatedSentences/verbSentences/detailedRemoval.txt','w') as details_removal, \
-         open('./updatedSentences/verbSentences/verbRemoval.p','wb') as tuple_write, \
-         open('./originalSentences/testingFile.txt','r') as f:
+    verb_dir = "./updatedSentences/verbSentences/"
+    with open(verb_dir + "errorSentences.txt","w") as err, \
+         open(verb_dir + "detailedRemoval.txt","w") as det, \
+         open(verb_dir + "verbRemoval.p","wb") as tup_write, \
+         open("./originalSentences/testingFile.txt","r") as f:
             for line in f:
-                line = line.rstrip('\n')
-                v_sentence_rem = verb_removal(line) ###change the name of this variable
-                if (v_sentence_rem[0] != "ERROR"):
-                    pickle.dump(v_sentence_rem, tuple_write)
-                    verb_pos = v_sentence_rem[2]
-                    verb_found = make_str(verb_pos)
-                    details_removal.write("Original Sentence: " + line + " ||| Word Removed: " + verb_found.rstrip('\n') + " ||| Updated Sentence: " + make_updated_sentence(line, verb_found,"").rstrip('\n') + " ||| POS Original Sentence: " +  tup_list_to_string(v_sentence_rem[0]) + " ||| POS Word Removed: " + tup_list_to_string(verb_pos) + " ||| POS Updated Sentence: " + tup_list_to_string(v_sentence_rem[1]) + '\n')
+                line = line.rstrip("\n")
+                line_wo_v = verb_removal(line)
+                if (line_wo_v[0] != "ERROR"):
+                    pickle.dump(line_wo_v, tup_write)
+                    verb_pos = line_wo_v[2]
+                    v_found = make_str(verb_pos)
+                    det.write(line + " ||| "
+                              + v_found.rstrip("\n") + " ||| "
+                              + make_upd_sentence(line, v_found,"").rstrip("\n") + " ||| "
+                              + tup_list_to_string(line_wo_v[0]) + " ||| "
+                              + tup_list_to_string(verb_pos) + " ||| "
+                              + tup_list_to_string(line_wo_v[1]) + "\n")
                 else:
-                    error_v.write(line + ' ||| POS Original Sentence: ' + tup_list_to_string(v_sentence_rem[1]) + '\n')
-#
-# # # THIS IS FOR NOUNS AND VERBS
+                    err.write(line + " ||| "
+                              + tup_list_to_string(line_wo_v[1]) + "\n")
+
 def noun_verb_textfile():
-    with open('./updatedSentences/nounverbSentences/errorSentences.txt','w') as error_v, \
-         open('./updatedSentences/nounverbSentences/detailedRemoval.txt','w') as details_removal, \
-         open('./updatedSentences/nounverbSentences/nounverbRemoval.p','wb') as tuple_write, \
-         open('./originalSentences/testingFile.txt','r') as f:
-                        for line in f:
-                            line = line.rstrip('\n')
-                            nv_sentence_rem = noun_verb_removal(line)
-                            if (nv_sentence_rem[0] != "ERROR"):
-                                noun_pos_list = nv_sentence_rem[2][0]
-                                verb_pos_list = nv_sentence_rem[2][1]
-                                pickle.dump((nv_sentence_rem[0],
-                                             nv_sentence_rem[1],
-                                             noun_pos_list
-                                             + verb_pos_list),
-                                            tuple_write)
-                                noun_str = make_str(noun_pos_list).rstrip('\n')
-                                verb_str = make_str(verb_pos_list).rstrip('\n')
-                                remove_noun_line = make_updated_sentence(line, noun_str,"noun_verb")
-                                remove_verb_line = make_updated_sentence(remove_noun_line, verb_str,"").rstrip('\n')
-                                details_removal.write("Original Sentence: " + line + " ||| Words Removed: " + noun_str + ' ' + verb_str + " ||| Updated Sentence: " + remove_verb_line + " ||| POS Original Sentence: " +  tup_list_to_string(nv_sentence_rem[0]) + " ||| POS Word Removed: " + tup_list_to_string(noun_pos_list + verb_pos_list) + " ||| POS Updated Sentence: " + tup_list_to_string(nv_sentence_rem[1]) + '\n')
-                            else:
-                                error_v.write(line + ' ||| POS Original Sentence: ' + tup_list_to_string(nv_sentence_rem[1]) + '\n')
+    nv_dir = "./updatedSentences/nounverbSentences/"
+    with open(nv_dir + "errorSentences.txt","w") as err, \
+         open(nv_dir + "detailedRemoval.txt","w") as det, \
+         open(nv_dir + "nounverbRemoval.p","wb") as tup_write, \
+         open("./originalSentences/testingFile.txt","r") as f:
+            for line in f:
+                line = line.rstrip("\n")
+                line_wo_nv = noun_verb_removal(line)
+                if (line_wo_nv[0] != "ERROR"):
+                    n_pos_lst = line_wo_nv[2][0]
+                    v_pos_lst = line_wo_nv[2][1]
+                    pickle.dump((line_wo_nv[0],
+                                 line_wo_nv[1],
+                                 n_pos_lst
+                                 + v_pos_lst),
+                                tup_write)
+                    noun_str = make_str(n_pos_lst).rstrip("\n")
+                    verb_str = make_str(v_pos_lst).rstrip("\n")
+                    rem_noun = make_upd_sentence(line, noun_str,"noun_verb")
+                    remove_verb = make_upd_sentence(rem_noun, verb_str,"")
+                    det.write(line + " ||| "
+                              + noun_str + ' ' + verb_str + " ||| "
+                              + remove_verb.rstrip("\n") + " ||| "
+                              + tup_list_to_string(line_wo_nv[0]) + " ||| "
+                              + tup_list_to_string(n_pos_lst + v_pos_lst) + " ||| "
+                              + tup_list_to_string(line_wo_nv[1]) + "\n")
+                else:
+                    err.write(line + " ||| "
+                              + tup_list_to_string(line_wo_nv[1]) + "\n")
+
+def subordinate_textfile():
+    sub_dir = "./updatedSentences/conjunctionSentences/"
+    with open(sub_dir + "errorSentences.txt","w") as err, \
+         open(sub_dir + "detailedRemoval.txt","w") as det, \
+         open(sub_dir + "nounverbRemoval.p","wb") as tup_write, \
+         open("./originalSentences/testingFile.txt","r") as f:
+            for line in f:
+                line = line.rstrip("\n")
+                line_wo_sub = subordinate_removal(line)
+                if (line_wo_sub != "ERROR"):
+                    pickle.dump(line_wo_sub, tup_write)
+                    sub_conj_lst = line_wo_sub[2]
+                    sub_found = make_str(sub_conj_lst)
+                    det.write(line + " ||| "
+                              + sub_found + " ||| "
+                              + make_upd_sentence(line,sub_found,"") + " ||| "
+                              + tup_list_to_string(line_wo_sub[0]) + " ||| "
+                              + tup_list_to_string(sub_conj_lst) + " ||| "
+                              + tup_list_to_string(line_wo_sub[1]) + "\n")
+                else:
+                    err.write(line + " ||| "
+                              + tup_list_to_string(line_wo_sub[1]) + "\n")
