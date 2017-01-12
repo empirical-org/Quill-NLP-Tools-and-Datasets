0ef07f5e5af810cb2a1f7eaf47bb43301f7ca3b1
calvarado <calvarado@wesleyan.edu>
2017-01-12 16:39:16 -0500

    I changed the regex used to find subodinate conjunctions. I also edited comments.

--- removingPOS/conjunction_remove.py
+++ removingPOS/conjunction_remove.py
@@ -1,27 +1,31 @@
 '''
 The main function in this file is subordinate_removal(s). This function
-handles sentences that contain subordinate conjunctions in them. It
-removes the subordinate conjunction from the sentence in the following
-ways: If a subordinate conjunction is in the beginnig of a sentence,
+handles sentences that contain subordinate conjunctions and removes
+parts of these sentences. The function removes the subordinate
+conjunctions in the following ways:
+- If a subordinate conjunction is in the beginning of a sentence,
 then it removes that word and all of the words following it until a
-comma is reached. In the sentence "After we went to the park, Billy
-went swimming." the string "After we went to the park," would be
-removed. In the sentence "I think we should go hiking after the
-football game" the words "after the football game" would be removed.
+comma is reached. For example, in the sentence "After we went to the
+park, Billy went swimming." the string "After we went to the park,"
+would be removed. If a sentence starts with a subordinate conjunction,
+but there is no comma in the sentence, then we do not remove anything
+from the sentence.
+- If a subordinate conjunction is in the middle of a sentence, then
+that word till the end of the string is removed. For example, in the
+sentence "I think we should go hiking after the football game." the
+words "after the football game" would be removed.
 Args:
-    s: A string that is a complete sentence. I will refer to the string
-    as a sentence below.
+    s: A string that is a complete sentence.
 Returns:
     A tuple with three inputs:
     The first input is list of tuples describing all of the words in
-    the sentence. Each tuple in this list contains the word,
-    dependency, and pos for every word in the sentence, note that
-    punctuation is also considered a word.
-    The second input contains a similar list as the first input in the
-    return statement, but it does not contain the subordinate conjunction
-    match that was found.
-    The third input is list of a list of tuples that contain the
-    subordinate conjunction match found.
+    the sentence. Each tuple in this list contains a word, which
+    could also be a punctuation mark, a dependency, and a pos for the
+    word. The second input contains a similar list as the first input,
+    but it does not contain the subordinate conjunction substring match.
+    The third input is list of tuples, similar to the list that was
+    described earlier, that contains the subordinate conjunction matches
+    found.
 '''
 import re
 import spacy
@@ -58,14 +62,16 @@ def subordinate_removal(s):
                                     'If|Once|Since|Than|That|Though|' \
                                     'Unless|Until|When|Whenever|Where|' \
                                     'Whereas|Wherever|Whether|While|Why|' \
-                                    'Even if|Even though|Provided that|' \
-                                    'Rather than|So that|In order that)(?=\s)')
+                                    'Even(?=\sif)|Even(?=\sthough)|' \
+                                    'Provided(?=\sthat)|Rather(?=\sthan)|' \
+                                    'So(?=\sthat))(?=\s)')
         conjunct_lower = re.compile('(?<=\s)(after|although|as|because|' \
                                     'before|if|once|since|than|that|though|' \
                                     'unless|until|when|whenever|where|' \
                                     'whereas|wherever|whether|while|why|' \
-                                    'even if|even though|provided that|' \
-                                    'rather than|so that|in order that)(?=\s)')
+                                    'even(?=\sif)|even(?=\sthough)|' \
+                                    'provided(?=\sthat)|rather(?=\sthan)|' \
+                                    'so(?=\sthat))(?=\s)')
         match_upper = conjunct_upper.search(s)
         match_lower = conjunct_lower.search(s)
         if (match_upper != None and s.find(",") > -1):
