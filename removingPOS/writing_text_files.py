8924006ab0c265081235a0bb843c24ddf053b1b5
calvarado <calvarado@wesleyan.edu>
2017-01-12 17:29:51 -0500

    I edited comments and changed some functions.

--- removingPOS/writing_text_files.py
+++ removingPOS/writing_text_files.py
@@ -1,79 +1,48 @@
 '''
 Most of the functions in this file write information to other files. The
 functions make use of noun_remove.py, verb_remove.py, noun_verb_remove.py,
-and ____________ to remove nouns, verbs, or subordinate conjunctions from
-sentences. Each function reads textfile data from the specified file
-located in the folder called orginalSentences and processes each line. I
-will explain each function in detail below:
+and conjunction_remove.py to remove nouns, verbs, or subordinate
+conjunctions from sentences. Each function below reads textfile data
+from the specified file located in the folder called orginalSentences
+and processes each line. I will explain each function in detail below:
 
 make_upd_sentence(s,match_found,removal_type):
-This function takes in a string and a list of tuples. It updates the
-string by removing any words that are in the list of tuples. For
-example, if thestring is "My family loves hiking." and the list of
-tuples is [('family', 'nsubj', 'NOUN'), ('went', 'ROOT', 'VERB')] then
-it returns "My hiking."
+This sentence removes any words from a string and outputs a string
+that looks like a sentence, meaning the first character in the
+string is capitalized and there is a period at the end.
 Args:
     s: A string that is a sentence.
-    match_found: An lst of words to remove. Check the comments on
-                 nlpSpacy.py to see what an lst is.
-    removal_type: This is the kind of removal that needs to be made and
-                  represents the pos of words found in match_found.
-                  Possible words are "noun", and "verb".
+    match_found: A substring that needs to be removed from s.
+    removal_type: This argument is only used by the function
+                  noun_verb_textfile.
 Return:
     This returns an updated string with the same words as s, but with
     words in match found removed.
+
+For example, if the string is "My family loves hiking." and the string we
+want to remove is "My family" then the function returns "Loves hiking."
 -----------------------------------------------------------------------
 * Refer to nlpSpacy.py to see what lst is. lst will be used here.
-The functions below handle writing information to two seperate files.
-Information is writtten to a textfile and another file ending in .p,
-which uses pickle. The textfile is for a person to understand how each
-line is processed. Each line in the textfile is in the following format:
- Original sentence: | Word removed: | Updated sentence: |
- POS lst for sentence | POS lst of removed words| POS lit of updated sentence
-The information written into the file ending in .p uses serialization.
-Here I write tuples, with three inputs, directly to the file. Each tuple
+The functions below handle writing information to three seperate files.
+Information is written to two textfile and a file ending in .p, which
+uses pickle.
+The textfile called detailedRemoval.txt is for a human to
+understand how each line was line was processed. Each line in the
+detailedRemoval.txt is in the following format:
+Original sentence: | Word removed: | Updated sentence: |
+POS lst for sentence | POS lst of removed words| POS lst of updated sentence
+The information written into the file ending in .p uses serialization. For
+each line I dump tuples into the file p. Each tuple has three inputs and
 contains information for a sentence that was processed. The first argument
 is the pos lst of the line being processed. The second input is a pos lst
-of the original sentence, but does not contain the speficied pos being
-removed. The third input is a lst of the words that were removed. I use
-serialization here so that others could easily reuse the final outputs
-of the data.
+of the original sentence, but with the pos matches removed. The third
+input is a lst of the words that were removed.
+The third file is called errorSentences.txt and contains the lst of
+sentences that could not be processed.
 -----------------------------------------------------------------------
-noun_textfile():
-This function reads from a texfile called nounScreening.txt in the folder
-originalSentences. It processes each line and removes the first noun or
-group of consecutive nouns found in the line. Each line is a complete
-sentence. Once a sentence is processed, it writes information to two
-seperate files.
-Args: None
-Return: Nothing is returned, instead information is written to other files.
-
-verb_textfile():
-This function reads from a texfile called verbScreening.txt in the folder
-originalSentences. It processes each line and removes the first verb or
-group of consecutive verbs found in the line. Each line is a complete
-sentence. Once a sentence is processed, it writes information to two
-seperate files.
-Args: None
-Return: Nothing is returned, instead information is written to other files.
-
-noun_verb_textfile():
-This function reads from a texfile called nounverbScreening.txt in the folder
-originalSentences. It processes each line and removes the first noun or
-group of consecutive nouns and the first verb or group of consecutive verbs
-found in the line. Each line is a complete sentence. Once a sentence is
-processed, it writes information to the two seperate files.
-Args: None
-Return: Nothing is returned, instead information is written to other files.
-
-subordinate_textfile():
-This function reads from a textfiles called subScreening.txt in the folder
-originalSentences. All of the sentences in subScreening contain subordinate
-conjunctions. Each line in the function subordinate_textfile() is
-processed and the function subordinate_removal removes subordinate
-conjunctions at the beggining of a sentence or in the middle of a sentence.
-Args: None
-Return: Nothing is returned, instead information is written to other files.
+The names of the functions below describe the kind of pos removal that
+was done. All of the outputs for the functions below follow the format
+described above.
 '''
 import pickle
 from nlpSpacy import *
@@ -82,38 +51,21 @@ from verb_remove import verb_removal
 from noun_verb_remove import noun_verb_removal
 from conjunction_remove import subordinate_removal
 
-def load(filename):
-    with open(filename, "rb") as f:
-        while True:
-            try:
-                yield pickle.load(f)
-            except EOFError:
-                break
-
-# items = load('./updatedSentences/nounSentences/nounRemoval.p')
-# lst = []
-# for i in items:
-#     lst.append(i)
-# print("At this point i should already have a list: ")
-# print(len(lst))
-# for tup in lst:
-#     print(tup)
-
-
 def make_upd_sentence(s,match_found,removal_type):
-    sentence_wo_noun = delete_words_string(s,match_found)
+    sentence_wo_match = delete_words_string(s,match_found)
     if (removal_type == "noun_verb"):
-        return sentence_wo_noun
+        return sentence_wo_match
     else:
-        return sentence_wo_noun[0].upper() + sentence_wo_noun[1:]
+        return sentence_wo_match[0].upper() + sentence_wo_match[1:]
 
 def noun_textfile():
     noun_dir = "./updatedSentences/nounSentences/"
     with open(noun_dir + "errorSentences.txt","w") as err, \
-         open(noun_dir + "./updatedSentences/nounSentences/detailedRemoval.txt","w") as det, \
-         open(noun_dir + "./updatedSentences/nounSentences/nounRemoval.p","wb") as tup_write, \
-         open("./originalSentences/testingFile.txt","r") as f:
+         open(noun_dir + "detailedRemoval.txt","w") as det, \
+         open(noun_dir + "nounRemoval.p","wb") as tup_write, \
+         open("./originalSentences/nounScreening.txt","r") as f:
             for line in f:
+                print(line)
                 line = line.rstrip("\n")
                 line_wo_n = noun_removal(line)
                 if (line_wo_n[0] != "ERROR"):
@@ -138,14 +90,16 @@ def verb_textfile():
     with open(verb_dir + "errorSentences.txt","w") as err, \
          open(verb_dir + "detailedRemoval.txt","w") as det, \
          open(verb_dir + "verbRemoval.p","wb") as tup_write, \
-         open("./originalSentences/testingFile.txt","r") as f:
+         open("./originalSentences/verbScreening.txt","r") as f:
             for line in f:
+                print(line)
                 line = line.rstrip("\n")
                 line_wo_v = verb_removal(line)
                 if (line_wo_v[0] != "ERROR"):
                     pickle.dump(line_wo_v, tup_write)
                     verb_pos = line_wo_v[2]
                     v_found = make_str(verb_pos)
+                    # print("This is the verb found: " + v_found)
                     det.write(line + " ||| "
                               + v_found.rstrip("\n") + " ||| "
                               + make_upd_sentence(line, v_found,"").rstrip("\n") + " ||| "
@@ -161,7 +115,7 @@ def noun_verb_textfile():
     with open(nv_dir + "errorSentences.txt","w") as err, \
          open(nv_dir + "detailedRemoval.txt","w") as det, \
          open(nv_dir + "nounverbRemoval.p","wb") as tup_write, \
-         open("./originalSentences/testingFile.txt","r") as f:
+         open("./originalSentences/nounverbScreening.txt","r") as f:
             for line in f:
                 line = line.rstrip("\n")
                 line_wo_nv = noun_verb_removal(line)
@@ -191,9 +145,10 @@ def subordinate_textfile():
     sub_dir = "./updatedSentences/conjunctionSentences/"
     with open(sub_dir + "errorSentences.txt","w") as err, \
          open(sub_dir + "detailedRemoval.txt","w") as det, \
-         open(sub_dir + "nounverbRemoval.p","wb") as tup_write, \
-         open("./originalSentences/testingFile.txt","r") as f:
+         open(sub_dir + "conjunctionRemoval.p","wb") as tup_write, \
+         open("./originalSentences/subScreening.txt","r") as f:
             for line in f:
+                print(line)
                 line = line.rstrip("\n")
                 line_wo_sub = subordinate_removal(line)
                 if (line_wo_sub != "ERROR"):
@@ -202,10 +157,16 @@ def subordinate_textfile():
                     sub_found = make_str(sub_conj_lst)
                     det.write(line + " ||| "
                               + sub_found + " ||| "
-                              + make_upd_sentence(line,sub_found,"") + " ||| "
+                              + line.replace(sub_found,"") + " ||| "
                               + tup_list_to_string(line_wo_sub[0]) + " ||| "
                               + tup_list_to_string(sub_conj_lst) + " ||| "
                               + tup_list_to_string(line_wo_sub[1]) + "\n")
                 else:
                     err.write(line + " ||| "
                               + tup_list_to_string(line_wo_sub[1]) + "\n")
+
+#Here I am calling the functions written above.
+noun_textfile()
+verb_textfile()
+noun_verb_textfile()
+subordinate_textfile()
