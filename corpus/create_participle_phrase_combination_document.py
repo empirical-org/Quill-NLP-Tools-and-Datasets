d32b5610cfbf2741506ffb59c91d2681a8e871c2
Max Buck <maxwellhigginsbuck@gmail.com>
2018-03-16 15:59:43 -0400

    edits to clean up possibly bad/useless sentences in create_participle_phrase_combination_document.py; newly generated participlePhraseSentencesAndTheirFragments.txt;

--- corpus/create_participle_phrase_combination_document.py
+++ corpus/create_participle_phrase_combination_document.py
@@ -1,14 +1,32 @@
 import os
 
-with open('participlePhraseSentencesAndTheirFragments.txt', 'a+') as \
+# dont allow square brackets or special characters that might make a phrase
+# appear oddly. Do not allow semi colons, but allow quotation marks (for now)
+acceptable_characters = set(
+        'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ \
+                -.,!?()\'$ *1234567890:&')
+
+with open('participlePhraseSentencesAndTheirFragments.txt', 'w+') as \
         output_file:
     for f in os.listdir('fragments'):
         input_filename = os.fsdecode(f)
-        if input_filename.startswith('participlePhrasesFrom'):
+        if input_filename.startswith('participlePhrasesFrom') and \
+                input_filename.endswith('.txt'):
             with open('fragments/' + input_filename, 'r') as input_file:
-                print(input_filename)
+                skip_next_line = False
                 for i, line in enumerate(input_file):
-                    if i % 8 == 0 or i % 8 == 1:
-                        output_file.write(line)
-
-
+                    line = " ".join(line.split()) # replace all whitespace w
+                    line = line.replace('_', '') # gutenberg underlines words
+                    line = line.strip() # trailing whitespace
+                    if skip_next_line:
+                        skip_next_line = False
+                        continue
+                    if i % 8 != 0 and i % 8 != 1:
+                        continue
+                    if not set(line).issubset(acceptable_characters) and i % 8 \
+                            == 0:
+                        skip_next_line = True
+                        continue
+                    # Do some cleanup on the line
+                    # sing space
+                    output_file.write(line + '\n')
