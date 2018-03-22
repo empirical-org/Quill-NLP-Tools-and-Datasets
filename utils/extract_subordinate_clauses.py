28e63a4562569a7f6af80bd798f18390c101946b
Max Buck <maxwellhigginsbuck@gmail.com>
2018-03-22 13:26:59 -0400

    subordinate clause extractor working, along with subordinate clause combine.py; ready to build model soon

--- utils/extract_subordinate_clauses.py
+++ utils/extract_subordinate_clauses.py
@@ -4,40 +4,130 @@ import spacy
 nlp = spacy.load('en')
 
 # Constants
-OUTPUT_FOLDER = 'fragments'
-OUTPUT_TEXT_FILE_BASE = OUTPUT_FOLDER + '/participlePhrasesFrom{}'
+OUTPUT_FOLDER = '../data/fragments/subordinate-clauses'
+OUTPUT_TEXT_FILE_BASE = OUTPUT_FOLDER + '/subordinateClausesFrom{}'
 CHUNK_SIZE = 1024
 
-def has_subordinate_clause(text):
+# TODO: read in chunks is a shared helper method
+def _read_in_chunks(file_object, chunk_size=CHUNK_SIZE):
+    """Generator to read a file piece by piece."""
+    while True:
+        data = file_object.read(chunk_size)
+        if not data:
+            break
+        yield data
+
+def _document_has_subordinating_conj(doc):
+    """Takes a spacy nlp document and returns a True if the document contains a
+    subordinate conjunction; else false"""
+    for word in doc:
+        if word.tag_ == 'IN':
+            return True
+    return False
+
+
+def _extract_subordinate_clause_type1(sentence):
+    """Return subordinate clause tuple from sentence with subordinate clause at
+    the beginning of the sentence."""
+    subordinate_clause = ''
+    subordinating_conj = None
+    flagged = False
+    for clause in sentence.split(','):
+        clause = clause.strip()
+        doc = nlp(clause)
+        if len(doc) < 1:
+            break
+        if not subordinating_conj and doc[0].tag_ == 'IN':
+            subordinating_conj = str(doc[0])
+            subordinate_clause += clause
+        elif subordinating_conj and doc[0].tag_ not in ['NN', 'NNS', 'NNP', 'NNPS', 'DT',
+                'JJ', 'JJR', 'JJS', 'PRP', 'PRP$']:
+            subordinate_clause += ', ' + clause
+            flagged = True
+        elif subordinating_conj:
+            break # clause followed by main clause
+    return subordinate_clause, subordinating_conj, flagged
+
+
+def _extract_subordinate_clause_type2(sentence):
+    """Return subordinate clause tuple from sentence with subordiante clause at
+    the end or middle of sentence."""
+    subordinate_clause = ''
+    subordinating_conj = None
+    flagged = False
+    for clause in sentence.split(','):
+        clause = clause.strip()
+        doc = nlp(clause)
+        if len(doc) < 1:
+            break
+        if not subordinating_conj and doc[0].tag_ == 'IN':
+            subordinating_conj = str(doc[0])
+            subordinate_clause += clause
+        elif subordinating_conj and doc[0].tag_ not in ['RB', 'RBR', 'RBS',
+                'VB', 'VBG', 'VBS', 'VBN', 'VBP', 'VBZ']:
+            subordinate_clause += ', ' + clause
+            flagged = True
+        elif subordinating_conj:
+            break # clause followed by main clause
+    return subordinate_clause, subordinating_conj, flagged
+
+
+def has_subordinate_clause(sentence):
     '''If this has a subordiate clause return an object with the clause,
     subordinate conjuction, and whether or not it is 'flagged' for review'''
-    result = {'clause':'', 'subordinating_conj':'', 'flagged':False}
-    text = text.strip()
+    result = {'clause': '', 'subordinating_conj': '', 'flagged': False}
+    sentence = sentence.strip()
     # Short circuit questions
-    if text[-1] = '?':
+    if len(sentence) == 0 or sentence[-1] == '?':
         return None
+
+    # TODO: does not cover clauses that begin with a relative (WP, WP$) pronoun
     
-    # If the text begins with subordinating conjunction (IN) and doesn't have a
-    # second clause that is complete... ug, will we have to go on a by clause
-    # basis
-
-    # TODO: pick up here tm
-
-    # IN, WP --> https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
-    # WP words are harder because they have more
-
+    # If subordinating conj is in the middle of the sentence:
+    # Start at subordinating conj and end with period or comma followed by
+    # adverb/verb/
+    # ie. Peter, with heroic unselfishness, did not say anything.
+    #     becomes,
+    #     with heroic unselfishness
+    #
+    # If sentence begins with subordingating conjuction:
+    # Start at subordinating conj and end with period or
+    # a comma followed by a noun/pronoun/adj/determiner
+    # ie. Until the end of time, he would love her.
+    #     becomes,
+    #     Until the end of time
+    # NOTE: With short introductory phrases, a comma is not always used. 
+    # For example,
+    # > After the holidays the girls went to the Blackheath High School.
+    # is a sentence that might be found in a book, still, we believe it would be
+    # better with a comma after holidays and would suggest the student add one.
+    #
+    doc = nlp(sentence)
+    if doc[0].tag_ == 'IN':
+        subordinate_clause, subordinating_conj, flagged =  _extract_subordinate_clause_type1(sentence)
+    elif _document_has_subordinating_conj(doc):
+        subordinate_clause, subordinating_conj, flagged =  _extract_subordinate_clause_type2(sentence)
+    else:
+        subordinating_conj = None
+            
+    if subordinating_conj:
+        result['clause'] = subordinate_clause
+        result['subordinating_conj'] = subordinating_conj
+        result['flagged'] = flagged
+    else:
+        result = None
 
     return result
 
 def write_sentences_with_subordinate_clauses(input_filename, output_filename):
     '''extract sentences with subordinate clauses and write them'''
     # open a working copy of the file to show its currently being written to
-    with open(input_file, 'r') as f:
+    with open(input_filename, 'r') as f:
         # final sentence may not be a complete sentence, save and prepend to next chunk
         leftovers = ''
         sentence_no = 0
-        output = open(output_file + '.working', 'w+')
-        for chunk in read_in_chunks(f): # lazy way of reading our file in case it's large
+        output = open(output_filename + '.working', 'w+')
+        for chunk in _read_in_chunks(f): # lazy way of reading our file in case it's large
             # prepend leftovers to chunk
             chunk = leftovers + chunk
             doc = nlp(chunk)
@@ -49,14 +139,14 @@ def write_sentences_with_subordinate_clauses(input_filename, output_filename):
                 leftovers = sents[-1]
             for sent in sents:
                 sent = sent.replace('\n', ' ')
-                clause = is_subordinate_clause(sent)
+                clause = has_subordinate_clause(sent)
                 if clause:
                     output.write("{}\n{}\n{}\n" \
                     "{}\n\n\n\n\n".format(sent, clause['clause'],
-                        phrase['subordinating_conj'], clause['flagged']))
+                        clause['subordinating_conj'], clause['flagged']))
         output.close()
         # remove the .working extention to show the file is finished
-        os.rename(output_file + '.working', output_file)
+        os.rename(output_filename + '.working', output_filename)
 
 def print_help():
     print('./extract_subordinate_clauses.py -f filename')
@@ -92,6 +182,6 @@ if __name__ == '__main__':
         input_filename = sys.argv[2]
         output_filename = OUTPUT_TEXT_FILE_BASE.format(input_filename.split('/')[-1])
         print(input_filename, output_filename)
-        write_sentences_with_participle_prhases(input_filename, output_filename)
+        write_sentences_with_subordinate_clauses(input_filename, output_filename)
     else:
         print_help()
