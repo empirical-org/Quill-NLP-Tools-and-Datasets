bb71a1c8086fe52b86b7bac320dca43f9f44a3c7
Max Buck <maxwellhigginsbuck@gmail.com>
2018-03-28 12:04:17 -0400

    version 0.0.12 of qfragment; this is a WORKING VERSION

--- utils/qfragment/setup.py
+++ utils/qfragment/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='qfragment',
-      version='0.0.11',
+      version='0.0.12',
       description='Sentence fragment detection and feedback',
       url='https://github.com/empirical-org/Quill-NLP-Tools-and-Datasets',
       author='Quill.org',
