fa9bc150033f923422e614be0dfbc93bf0d6115f
Max Buck <maxwellhigginsbuck@gmail.com>
2018-04-03 14:04:57 -0400

    bump version; name egg in setup.py (should fix heroku...)

--- utils/qfragment/setup.py
+++ utils/qfragment/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='qfragment',
-      version='0.0.17',
+      version='0.0.18',
       description='Sentence fragment detection and feedback',
       url='https://github.com/empirical-org/Quill-NLP-Tools-and-Datasets',
       author='Quill.org',
@@ -17,7 +17,7 @@ setup(name='qfragment',
           'tflearn',
       ],
       dependency_links = [
-          'https://github.com/explosion/spacy-models/releases/download/en_core_web_lg-2.0.0/en_core_web_lg-2.0.0.tar.gz'
+          'https://github.com/explosion/spacy-models/releases/download/en_core_web_lg-2.0.0/en_core_web_lg-2.0.0.tar.gz#egg=en_core_web_lg==2.0.0'
       ],
       include_package_data=True,
       zip_safe=False)
