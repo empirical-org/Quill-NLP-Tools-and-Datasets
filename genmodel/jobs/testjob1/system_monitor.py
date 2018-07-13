b8cafd6acff525f42ab8926e359952efb3ca0a32
Max Buck <maxwellhigginsbuck@gmail.com>
2018-07-13 16:14:59 -0400

    fixes for wack stuff

--- genmodel/jobs/testjob1/system_monitor.py
+++ genmodel/jobs/testjob1/system_monitor.py
@@ -2,17 +2,18 @@ import logging
 import psutil
 import socket
 import time
+import os
 
 FNAME=os.path.basename(__file__)
 PID=os.getpid()
 HOST=socket.gethostname()
 
 # set up logging
-log_filename='reducer_{}.log'.format(os.getpid())
+log_filename='sysmonitor_{}.log'.format(os.getpid())
 log_format = '%(levelname)s %(asctime)s {pid} {filename} %(lineno)d %(message)s'.format(
         pid=PID, filename=FNAME)
 logging.basicConfig(format=log_format,
-    filename='/var/log/reducerlogs/{}'.format(log_filename),
+    filename='/var/log/systemmonitorlogs/{}'.format(log_filename),
     datefmt='%Y-%m-%dT%H:%M:%S%z',
     level=logging.INFO)
 logger = logging.getLogger('reducer')
