e31da4c721dd6a2c02af1c3bfd618a5d75540dc5
Max Buck <maxwellhigginsbuck@gmail.com>
2018-07-18 14:54:12 -0400

    wizardry

--- utils/modelgen/spell-training/train.py
+++ utils/modelgen/spell-training/train.py
@@ -8,38 +8,42 @@ import tflearn
 from tflearn.data_utils import to_categorical
 import gc
 
-# Fetching data from API #######################################################
 
-API_URL = "http://127.0.0.1:5000" # Currently set to localhost
-JOB_ID = 139
-JOB_URL = API_URL + '/' + str(JOB_ID)
+try:
+    SECRET = os.environ['SECRET']
+    JOB_ID = os.environ['JOB_ID']
+    API_URL = os.environ['API_URL']
+    JOB_URL = '{}/jobs/{}/vectors'.format(API_URL, JOB_ID)
+    MODEL_NAME = os.environ.get('GENERATED_MODEL_NAME','{}.tfl'.format(JOB_ID))
+except KeyError as e:
+    print('critical environment variables were not set! exiting.')
+    raise e
 
-# Returns list of labeled_vectors from API, each training_example is (deflated_vector, label)
-# Returned object is in format: [({5:1, 6:2, 5000:1}, 1), ...]
-# TODO: Set up pagination using offset and limit here. Will pass as params in the request.
 def get_labeled_vectors(offset, limit):
+    '''Returns list of labeled_vectors from API, each training_example is
+    (deflated_vector, label) Returned object is in format: [({5:1, 6:2, 5000:1},
+        1), ...]'''
+
     payload = {'offset': offset, 'limit': limit, 'full_info': 0}
     r = requests.get(url=JOB_URL, params=payload)
-    examples_json = r.json()['labeled_vector']
-    return [(example['vector'], example['label']) for example in examples_json]
+    return r.json()['labeled_vectors']
 
 def get_model_info():
     payload = {'offset': 0, 'limit': 0, 'full_info': 1}
     r = requests.get(url=JOB_URL, params=payload)
     info_json = r.json()
-    return info_json['input_vector_length'], info_json['num_classes'], info_json['num_examples']
+    return (info_json['vector_length'], info_json['num_classes'],
+            info_json['num_labeled_vectors'])
 
-# Given a deflated_vector (dictionary), return its vector (sparse np array)
 def inflate(deflated_vec, vec_len):
+    '''Given a deflated_vector (dictionary), return its vector (sparse np
+    array)'''
     result = np.zeros(vec_len)
     for idx, count in deflated_vec.items():
         result[int(idx)] = count
     return result
 
 
-# Building, training TF Model ##################################################
-
-# Builds the model, given proper parameters
 def build_model(input_vec_length, num_classes):
     tf.reset_default_graph() # This resets all parameters and variables, leave this here
     net = tflearn.input_data([None, input_vec_length])                # Input
@@ -50,7 +54,6 @@ def build_model(input_vec_length, num_classes):
     return model
 
 
-# Trains model in successive slabs by pulling slabs of data from the API
 def train_model(model, num_classes, slab_size, save_file):
     print("Training TF model...")
     _, _, num_examples = get_model_info()
@@ -59,8 +62,8 @@ def train_model(model, num_classes, slab_size, save_file):
         end_pos = min(start_pos + slab_size, num_examples)
         examples = get_labeled_vectors(start_pos, slab_size)
         print('Fitting model with start: {}, end: {}'.format(start_pos, end_pos))
-        x_train = np.asarray([inflate(vec['indices'], vec['reductions']) for vec, _ in examples])
-        y_train = to_categorical(np.asarray([lab for _, lab in examples]), num_classes)
+        x_train = np.asarray([inflate(e['vector']['indices'], e['vector']['reductions']) for e in examples])
+        y_train = to_categorical(np.asarray([e['label'] for e in examples]), num_classes)
         model.fit(x_train, y_train, validation_set=0.1, show_metric=True, batch_size=128, n_epoch=5)
 
         del examples
@@ -77,8 +80,10 @@ def train_model(model, num_classes, slab_size, save_file):
     print('Success! Your model was built and saved.')
     return model
 
-SAVE_FILE = 'test-model.tfl'
-INPUT_VEC_LEN, NUM_CLASSES, _ = get_model_info()
-print("Building Tensorflow model...")
-model = build_model(input_vec_length=INPUT_VEC_LEN, num_classes=NUM_CLASSES)
-model = train_model(model, NUM_CLASSES, slab_size=30000, save_file=SAVE_FILE)
+
+if __name__ == '__main__':
+    INPUT_VEC_LEN, NUM_CLASSES, _ = get_model_info()
+    print("Building Tensorflow model...")
+    model = build_model(input_vec_length=INPUT_VEC_LEN, num_classes=NUM_CLASSES)
+    model = train_model(model, NUM_CLASSES, slab_size=30000,
+            save_file=MODEL_NAME)
