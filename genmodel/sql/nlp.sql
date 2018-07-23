1153bfdccb815b78026bc1d4f65d483b785e275e
Max Buck <maxwellhigginsbuck@gmail.com>
2018-07-23 10:34:30 -0400

    add update triggers on db (sql file); add new field 'hash' to jobs

--- genmodel/sql/nlp.sql
+++ genmodel/sql/nlp.sql
@@ -2,7 +2,8 @@
 /* Create Jobs Table */
 CREATE TABLE IF NOT EXISTS jobs (
   id serial PRIMARY KEY NOT NULL,
-  name varchar UNIQUE,
+  name varchar,
+  hash varchar UNIQUE,
   state varchar, -- created, running, finished, 
   created timestamp with time zone default now(),
   updated timestamp with time zone default now(),
@@ -79,3 +80,42 @@ CREATE TABLE IF NOT EXISTS vectors (
   meta jsonb
 );
 
+
+
+/* Triggers and Functions */
+
+
+CREATE FUNCTION row_updated() RETURNS TRIGGER
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+  NEW.updated := current_date;
+  RETURN NEW;
+END;
+$$;
+
+
+CREATE TRIGGER trigger_job_update
+  BEFORE UPDATE ON jobs
+  FOR EACH ROW
+  EXECUTE PROCEDURE row_updated();
+
+CREATE TRIGGER trigger_droplet_update
+  BEFORE UPDATE ON droplets
+  FOR EACH ROW
+  EXECUTE PROCEDURE row_updated();
+
+CREATE TRIGGER trigger_labeled_data_update
+  BEFORE UPDATE ON labeled_data
+  FOR EACH ROW
+  EXECUTE PROCEDURE row_updated();
+
+CREATE TRIGGER trigger_reduction_update
+  BEFORE UPDATE ON reductions
+  FOR EACH ROW
+  EXECUTE PROCEDURE row_updated();
+
+CREATE TRIGGER trigger_vector_update
+  BEFORE UPDATE ON vectors
+  FOR EACH ROW
+  EXECUTE PROCEDURE row_updated();
