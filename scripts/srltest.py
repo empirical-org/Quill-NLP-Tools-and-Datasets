e481410a8b352ad993ad87050fb018eb45806b75
yvespeirsman <yves.peirsman@gmail.com>
2019-03-19 19:58:06 +0100

    Added support for auxiliary verbs

--- scripts/srltest.py
+++ scripts/srltest.py
@@ -1,4 +1,4 @@
-from allennlp.predictors.predictor import Predictor
+#from allennlp.predictors.predictor import Predictor
 import json
 from tqdm import tqdm
 import re
@@ -27,37 +27,64 @@ def perform_srl():
 
 
 def parse_srl(f):
+
+    modal_verbs = set(["will", "shall", "may", "might", "can", "could", "must", "ought to",
+                       "should", "would", "used to", "need"])
+
+    aux_verbs = set(["do", "does"])
+
     all_sentences = []
     with open(f) as i:
         d = json.load(i)
         for sent in d:
-            sentence_info = []
-            sentence_info.append([" ".join(sent["words"][11:])])
+            sentence_info = {"sentence": " ".join(sent["words"][11:]),
+                             "meta": [],
+                             "verbs": []}
+
+            auxiliary = None
             for verb in sent["verbs"]:
-                verb_string = verb["verb"]
-                arg0_string = " "
-                arg1_string = " "
-                arg2_string = " "
+                verb_string = verb["verb"].lower()
 
-                arg0 = re.search("\[ARG0: (.*?)\]", verb["description"])
-                if arg0:
-                    arg0_string = arg0.group(1)
-                arg1 = re.search("\[ARG1: (.*?)\]", verb["description"])
-                if arg1:
-                    arg1_string = arg1.group(1)
+                if verb_string in modal_verbs:
+                    sentence_info["meta"].append("modal")
+                    auxiliary = verb_string
 
-                arg2 = re.search("\[ARG2: (.*?)\]", verb["description"])
-                if arg2:
-                    arg2_string = arg2.group(1)
+                elif verb_string in aux_verbs:
+                    sentence_info["meta"].append("auxiliary")
+                    auxiliary = verb_string
 
-                sentence_info.append([verb_string, arg0_string, arg1_string, arg2_string])
+                else:
+                    arg0_string = "-"
+                    arg1_string = "-"
+                    arg2_string = "-"
+
+                    arg0 = re.search("\[ARG0: (.*?)\]", verb["description"])
+                    if arg0:
+                        arg0_string = arg0.group(1)
+                    arg1 = re.search("\[ARG1: (.*?)\]", verb["description"])
+                    if arg1:
+                        arg1_string = arg1.group(1)
+
+                    arg2 = re.search("\[ARG2: (.*?)\]", verb["description"])
+                    if arg2:
+                        arg2_string = arg2.group(1)
+
+                    if auxiliary is not None:
+                        verb_string += f" ({auxiliary})"
+                        auxiliary = None
+
+                    sentence_info["verbs"].append([verb_string, arg0_string, arg1_string, arg2_string])
 
             all_sentences.append(sentence_info)
 
     for sentence in all_sentences:
-        flat_list = [i for l in sentence for i in l]
-        print("\t".join(flat_list))
+        output_list = [sentence["sentence"], " ".join(sentence["meta"][1:])]  # strip the first "meta" label: main clause
+        for verb in sentence["verbs"]:
+            for item in verb:
+                output_list.append(item)
+
+        print("\t".join(output_list))
 
 
-perform_srl()
-parse_srl("output.json")
+#perform_srl()
+parse_srl("data/srl_output.json")
