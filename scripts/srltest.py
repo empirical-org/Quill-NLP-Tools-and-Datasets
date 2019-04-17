df27948746cf356c9db16e04978f5a99e2ac276f
yvespeirsman <yves.peirsman@gmail.com>
2019-04-17 17:38:26 +0200

    Added first basic version of argument clustering

--- scripts/srltest.py
+++ scripts/srltest.py
@@ -2,6 +2,10 @@ from allennlp.predictors.predictor import Predictor
 import json
 from tqdm import tqdm
 import re
+from sklearn.cluster import KMeans
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+from collections import defaultdict
 
 
 def perform_srl(f, sentence_start):
@@ -25,13 +29,41 @@ def perform_srl(f, sentence_start):
     return full_output
 
 
+def group_by_main_verb(sentences):
+
+    groups = defaultdict(list)
+    for sentence in sentences:
+        if len(sentence["verbs"]) > 3:
+            main_verb = sentence["verbs"][3][0]
+            groups[main_verb].append(sentence)
+        else:
+            groups["-"].append(sentence)
+
+    return groups
+
+
+def cluster(list_of_texts):
+    pipeline = Pipeline([
+        ("vect", CountVectorizer()),
+        ("tfidf", TfidfTransformer()),
+        ("clust", KMeans(n_clusters=3))
+    ])
+
+    try:
+        clusters = pipeline.fit_predict(list_of_texts)
+    except ValueError:
+        clusters = list(range(len(list_of_texts)))
+    return clusters
+
+
 def identify_argument(verb, argument_number):
     arg0 = re.search(f"\[ARG{argument_number}: (.*?)\]", verb["description"])
     if arg0:
         return arg0.group(1)
     return "-"
 
-def parse_srl_output(srl_output_list, sentence_start):
+
+def parse_srl_output(srl_output_list):
 
     modal_verbs = set(["will", "shall", "may", "might", "can", "could", "must", "ought to",
                        "should", "would", "used to", "need"])
@@ -70,8 +102,24 @@ def parse_srl_output(srl_output_list, sentence_start):
 
         all_sentences.append(sentence_info)
 
-    all_sentences.sort(key = lambda x: x["verbs"][3][0] if len(x["verbs"]) > 3 else "")
+    main_verb_groups = group_by_main_verb(all_sentences)
+    for main_verb in main_verb_groups:
+        texts = []
+        for sentence in main_verb_groups[main_verb]:
+            if len(sentence["verbs"]) > 3:
+                texts.append(" ".join(sentence["verbs"][3]))
+            else:
+                texts.append("")
 
+        text_clusters = cluster(texts)
+
+        print("--")
+        print(main_verb)
+        for (sentence, c) in zip(main_verb_groups[main_verb], text_clusters):
+            print(c, sentence["response"])
+
+
+    """
     for sentence in all_sentences:
         output_list = [sentence["response"], " ".join(sentence["meta"][1:])]  # strip the first "meta" label: main clause
         for verb in sentence["verbs"][3:]:
@@ -79,9 +127,9 @@ def parse_srl_output(srl_output_list, sentence_start):
                 output_list.append(item)
 
         print("\t".join(output_list))
-
+    """
 
 sentence_start = "Schools should not allow junk food to be sold on campus "
 
 srl_out = perform_srl("scripts/data/sentences.txt", sentence_start)
-parse_srl_output(srl_out, sentence_start)
+parse_srl_output(srl_out)
