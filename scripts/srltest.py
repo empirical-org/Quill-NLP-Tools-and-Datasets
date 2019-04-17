c922b1614f1b38d9127d681878ba8348faf3156e
Yves Peirsman <yvespeirsman@gmail.com>
2019-04-17 16:04:33 +0200

    Added batch processing of input sentences

--- scripts/srltest.py
+++ scripts/srltest.py
@@ -1,32 +1,37 @@
-#from allennlp.predictors.predictor import Predictor
+from allennlp.predictors.predictor import Predictor
 import json
 from tqdm import tqdm
 import re
 
 
-def perform_srl():
+def perform_srl(f, sentence_start):
 
     predictor = Predictor.from_path("https://s3-us-west-2.amazonaws.com/allennlp/models/srl-model-2018.05.25.tar.gz")
-    sentence_start = "Schools should not allow junk food to be sold on campus "
 
-    sentences = []
-    with open("sentences.txt") as i:
+    sentences, responses = [], []
+    with open(f) as i:
       for line in i:
         line = line.strip()
         sentence = sentence_start + line
-        sentences.append(sentence)
+        responses.append(line)
+        sentences.append({"sentence": sentence})
 
-    # TODO: Prediction is much more efficient if we feed the sentences as a batch.
-    output = []
-    for sentence in tqdm(sentences):
-        x = predictor.predict(sentence)
-        output.append(x)
+    output = predictor.predict_batch_json(sentences)
 
-    with open("output.json", "w") as o:
-        json.dump(output, o)
+    full_output = [{"sentence": sentence_start + response,
+                    "response": response,
+                    "srl": srl} for (response, srl) in zip(responses, output)]
+
+    return full_output
 
 
-def parse_srl(f):
+def identify_argument(verb, argument_number):
+    arg0 = re.search(f"\[ARG{argument_number}: (.*?)\]", verb["description"])
+    if arg0:
+        return arg0.group(1)
+    return "-"
+
+def parse_srl_output(srl_output_list, sentence_start):
 
     modal_verbs = set(["will", "shall", "may", "might", "can", "could", "must", "ought to",
                        "should", "would", "used to", "need"])
@@ -34,57 +39,49 @@ def parse_srl(f):
     aux_verbs = set(["do", "does"])
 
     all_sentences = []
-    with open(f) as i:
-        d = json.load(i)
-        for sent in d:
-            sentence_info = {"sentence": " ".join(sent["words"][11:]),
-                             "meta": [],
-                             "verbs": []}
+    for sent in srl_output_list:
+        sentence_info = {"sentence": sent["sentence"],
+                         "response": sent["response"],
+                         "meta": [],
+                         "verbs": []}
 
-            auxiliary = None
-            for verb in sent["verbs"]:
-                verb_string = verb["verb"].lower()
+        auxiliary = None
+        for verb in sent["srl"]["verbs"]:
+            verb_string = verb["verb"].lower()
 
-                if verb_string in modal_verbs:
-                    sentence_info["meta"].append("modal")
-                    auxiliary = verb_string
+            if verb_string in modal_verbs:
+                sentence_info["meta"].append("modal")
+                auxiliary = verb_string
 
-                elif verb_string in aux_verbs:
-                    sentence_info["meta"].append("auxiliary")
-                    auxiliary = verb_string
+            elif verb_string in aux_verbs:
+                sentence_info["meta"].append("auxiliary")
+                auxiliary = verb_string
 
-                else:
-                    arg0_string = "-"
-                    arg1_string = "-"
-                    arg2_string = "-"
+            else:
+                arg0_string = identify_argument(verb, 0)
+                arg1_string = identify_argument(verb, 1)
+                arg2_string = identify_argument(verb, 2)
 
-                    arg0 = re.search("\[ARG0: (.*?)\]", verb["description"])
-                    if arg0:
-                        arg0_string = arg0.group(1)
-                    arg1 = re.search("\[ARG1: (.*?)\]", verb["description"])
-                    if arg1:
-                        arg1_string = arg1.group(1)
+                if auxiliary is not None:
+                    verb_string += f" ({auxiliary})"
+                    auxiliary = None
 
-                    arg2 = re.search("\[ARG2: (.*?)\]", verb["description"])
-                    if arg2:
-                        arg2_string = arg2.group(1)
+                sentence_info["verbs"].append([verb_string, arg0_string, arg1_string, arg2_string])
 
-                    if auxiliary is not None:
-                        verb_string += f" ({auxiliary})"
-                        auxiliary = None
+        all_sentences.append(sentence_info)
 
-                    sentence_info["verbs"].append([verb_string, arg0_string, arg1_string, arg2_string])
-
-            all_sentences.append(sentence_info)
+    all_sentences.sort(key = lambda x: x["verbs"][3][0] if len(x["verbs"]) > 3 else "")
 
     for sentence in all_sentences:
-        output_list = [sentence["sentence"], " ".join(sentence["meta"][1:])]  # strip the first "meta" label: main clause
-        for verb in sentence["verbs"]:
+        output_list = [sentence["response"], " ".join(sentence["meta"][1:])]  # strip the first "meta" label: main clause
+        for verb in sentence["verbs"][3:]:
             for item in verb:
                 output_list.append(item)
 
         print("\t".join(output_list))
 
 
-#perform_srl()
-parse_srl("data/srl_output.json")
+sentence_start = "Schools should not allow junk food to be sold on campus "
+
+srl_out = perform_srl("scripts/data/sentences.txt", sentence_start)
+parse_srl_output(srl_out, sentence_start)
