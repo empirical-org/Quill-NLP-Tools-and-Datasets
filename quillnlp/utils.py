6761896601e91d9f3fa9eefd48497ba3abf86c76
yvespeirsman <yves.peirsman@gmail.com>
2019-05-17 15:27:40 +0200

    Added Notebook with coreference evaluation

--- quillnlp/utils.py
+++ quillnlp/utils.py
@@ -8,3 +8,27 @@ def detokenize(s):
     s = re.sub("\s+(n't)", "\\1", s)
     return s
 
+
+import re
+
+
+def extract_referents_from_xml_tagged_strings(strings):
+    re_tagged_referent = re.compile(
+        r"<ref[ ]+id=(?P<id>(\?|-?\d+))[ ]*>(?P<string>.+?)</ref>")
+    for string in strings:
+        # print(string)
+        spans = []
+        s_scrubbed = string
+        while True:
+            m = re_tagged_referent.search(s_scrubbed)
+            if not m:
+                break
+            matched_string = m.group("string")
+            matched_id = m.group("id")
+            s_scrubbed = s_scrubbed[:m.start(0)] + matched_string + s_scrubbed[m.end(0):]
+            spans.append({"start": m.start(),
+                          "end": m.start() + len(matched_string),
+                          "str_": matched_string,
+                          "id": matched_id})
+        print(spans)
+        yield {"text": s_scrubbed, "refs": spans}
--- tests/test_nlp_utils.py
+++ tests/test_nlp_utils.py
@@ -1,4 +1,5 @@
 from quillnlp.utils import detokenize
+from quillnlp.utils import extract_referents_from_xml_tagged_strings
 
 
 def test_detokenization1():
@@ -17,3 +18,31 @@ def test_detokenization2():
 
     detokenized_sentence = detokenize(sentence_in)
     assert detokenized_sentence == sentence_out
+
+
+def test_extract_referents_from_xml_tagged_strings():
+    s = ("<ref id=0>Schools</ref> should not allow <ref id=1>junk food</ref> "
+         "to be sold on campus but <ref id=1>it</ref> generates money for schools")
+    assert (
+            list(extract_referents_from_xml_tagged_strings([s]))
+            == [{'text': 'Schools should not allow junk food to be sold on campus but it generates money for schools',
+                 'refs': [
+                     {'start': 0, 'end': 7, 'str_': 'Schools', 'id': '0'},
+                     {'start': 25, 'end': 34, 'str_': 'junk food', 'id': '1'},
+                     {'start': 60, 'end': 62, 'str_': 'it', 'id': '1'}]}]
+    )
+
+
+def test_extract_referents_from_xml_tagged_strings2():
+    s = ("<ref id=0>Schools</ref> should not allow <ref id=1>junk food</ref> "
+        "to be sold on campus because <ref id=0>they</ref> should provide more "
+        "healthy options for students to choose")
+    assert (
+            list(extract_referents_from_xml_tagged_strings([s]))
+            == [{'text': 'Schools should not allow junk food to be sold on campus because they should provide more healthy options for students to choose',
+                 'refs': [
+                     {'start': 0, 'end': 7, 'str_': 'Schools', 'id': '0'},
+                     {'start': 25, 'end': 34, 'str_': 'junk food', 'id': '1'},
+                     {'start': 64, 'end': 68, 'str_': 'they', 'id': '0'}]}]
+    )
+
