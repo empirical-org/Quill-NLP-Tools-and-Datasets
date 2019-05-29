60e04bb65a4577fe03de2561863763de089c6646
yvespeirsman <yves.peirsman@gmail.com>
2019-05-29 12:26:22 +0200

    Updated D3 visualization with coreference and classifier labels

--- demo/app.py
+++ demo/app.py
@@ -3,14 +3,8 @@ from flask import Flask, render_template
 
 app = application = Flask(__name__, static_url_path='/static')
 
-topic_file = "demo/static/topics.json"
 
 @app.route("/")
 def matches():
 
-    with open(topic_file) as i:
-        topics = json.load(i)
-
-    topics = [str(topic[0]) + ": " + topic[1] for topic in topics]
-
-    return render_template("index.html", topics=topics)
+    return render_template("index.html")
--- scripts/srltest.py
+++ scripts/srltest.py
@@ -1,25 +1,46 @@
 import json
 import spacy
-from quillnlp.utils import detokenize
-from quillnlp.preprocess import lemmatize, postag
-from quillnlp.cluster import cluster
 import re
 from collections import defaultdict
+
+from allennlp.models.archival import load_archive
+
+from quillnlp.utils import detokenize
+from quillnlp.preprocess import lemmatize, postag
 from quillnlp.topics import find_topics, get_topics_in_document, print_topics
+from quillnlp.models.basic_topic_classifier import BasicTopicClassifier
+from quillnlp.dataset_readers.quill_responses import QuillResponsesDatasetReader
+from quillnlp.predictors.topic import TopicPredictor
+
 
 from allennlp.predictors.predictor import Predictor
 
 predictor = Predictor.from_path("https://s3-us-west-2.amazonaws.com/allennlp/models/coref-model-2018.02.05.tar.gz")
 en = spacy.load("en")
 
+VERB_MAP = {"s": "be",
+            "is": "be",
+            "ca": "can",
+            "wo": "will"}
+
+SUBJECTS_TO_IGNORE = set(["I", "some", "some people"])
+
+CLASSIFIER_FILE = "models/junkfood_but_topic_classifier_glove.tar.gz"
+
 
 def group_by_main_verb(sentences):
+    """
+    Groups the sentences by their main verb and its arg0. This is needed to produce the
+    clustered data for the D3 visualization.
+    :param sentences:
+    :return: a dictionary of the form {arg0: {main_verb: [sentence1, sentence2, ...]}}
+    """
 
     groups = {"-": {"-": []}}
     for sentence in sentences:
         if len(sentence["verbs"]) > 3:
             main_verb = sentence["verbs"][3][0].split("(")[0].strip()  # verbs are strings like "allow (should)"
-            main_verb_arg0 = sentence["verbs"][3][1].lower()
+            main_verb_arg0 = sentence["verbs"][3][2].lower()
             if main_verb_arg0 not in groups:
                 groups[main_verb_arg0] = {}
             if main_verb not in groups[main_verb_arg0]:
@@ -28,15 +49,18 @@ def group_by_main_verb(sentences):
         else:
             groups["-"]["-"].append(sentence)
 
-    #print(json.dumps(groups, indent=4))
-    #input()
     return groups
 
 
 def identify_argument(verb, argument_number, words):
+    """
+    Identifies a given argument number of a verb. Returns its string and start+end
+    indexes. This allows easier integration of the semantic role labelling and
+    coreference results.
+    """
     indexes = []
     arg_tokens = []
-    #print("V", verb)
+
     tags_words = list(zip(verb["tags"], words))
     for idx, (tag, word) in enumerate(tags_words):
         if tag.endswith(f"ARG{argument_number}"):
@@ -51,11 +75,6 @@ def identify_argument(verb, argument_number, words):
     else:
         return "-", []
 
-    """
-    arg0 = re.search(f"\[ARG{argument_number}: (.*?)\]", verb["description"])
-    if arg0:
-        return arg0.group(1)
-    """
     return "-"
 
 
@@ -85,6 +104,65 @@ def get_coreference_dictionary(sentence):
     return corefs
 
 
+def create_visualization_data(all_sentences, output_file):
+    """
+    Creates a json file with the data used by the D3 visualization.
+    :param all_sentences: the output of our SRL + coreference analysis
+    :param output_file: the json file where we will dump the results
+    :return:
+    """
+
+    archive = load_archive(CLASSIFIER_FILE)
+    predictor = Predictor.from_archive(archive, 'topic_classifier')
+
+
+    output = {"name": prompt,
+              "children": []}
+
+    grouped_responses = group_by_main_verb(all_sentences)
+    for main_subj in grouped_responses:
+        print("--")
+        print(main_subj)
+
+        main_subject_branch = {"name": main_subj,
+                               "children": []}
+
+        for main_verb in grouped_responses[main_subj]:
+            print("-", main_verb)
+            topic_dictionary = defaultdict(list)
+            main_verb_branch = {"name": main_verb,
+                                "children": []}
+            for sentence in grouped_responses[main_subj][main_verb]:
+                if len(sentence["verbs"]) > 3:
+                    argument_strings = []
+                    for verb_frame in sentence["verbs"][3:]:
+                        argument_strings += verb_frame[1:]
+
+
+                    result = predictor.predict_json({"text": sentence["response"]})
+                    #print(input["text"], "\nC:", input["label"], "\nP:", result["label"])
+                    best_topic = result["label"]
+
+                    topic_dictionary[best_topic].append(sentence["response"])
+
+            for topic in topic_dictionary:
+                topic_branch = {"name": topic,
+                                "children": []}
+
+                for response in topic_dictionary[topic]:
+                    topic_branch["children"].append({"name": response,
+                                                    "value": 1})
+
+                main_verb_branch["children"].append(topic_branch)
+
+            main_subject_branch["children"].append(main_verb_branch)
+
+        output["children"].append(main_subject_branch)
+
+    with open(output_file, "w") as o:
+        json.dump(output, o)
+
+
 def parse_srl_output(srl_output_list, topic_model, dictionary):
 
     modal_verbs = set(["will", "shall", "may", "might", "can", "could", "must", "ought to",
@@ -106,6 +184,7 @@ def parse_srl_output(srl_output_list, topic_model, dictionary):
         auxiliary = None
         for verb in sent["srl"]["verbs"]:
             verb_string = verb["verb"].lower()
+            verb_string = VERB_MAP.get(verb_string, verb_string)
 
             if verb_string in modal_verbs:
                 sentence_info["meta"].append("modal")
@@ -134,77 +213,16 @@ def parse_srl_output(srl_output_list, topic_model, dictionary):
                     verb_string += f" ({auxiliary})"
                     auxiliary = None
 
-                if arg0_string != "I":  # Exclude constructions like "I think", "I guess", etc.
+                if arg0_string not in SUBJECTS_TO_IGNORE:  # Exclude constructions like "I think", "I guess", etc.
                     sentence_info["verbs"].append([verb_string, arg0_string, arg0_antecedent, arg1_string, arg2_string])
 
         all_sentences.append(sentence_info)
 
-    output = {"name": prompt,
-              "children": []}
-
-    grouped_responses = group_by_main_verb(all_sentences)
-    for main_subj in grouped_responses:
-        print("--")
-        print(main_subj)
-
-        main_subject_branch = {"name": main_subj,
-                               "children": []}
-
-        for main_verb in grouped_responses[main_subj]:
-            print("-", main_verb)
-            topic_dictionary = defaultdict(list)
-            main_verb_branch = {"name": main_verb,
-                                "children": []}
-            for sentence in grouped_responses[main_subj][main_verb]:
-                if len(sentence["verbs"]) > 3:
-                    argument_strings = []
-                    for verb_frame in sentence["verbs"][3:]:
-                        argument_strings += verb_frame[1:]
-                    text = " ".join(argument_strings)
-
-                    lemmas = lemmatize(text)
-                    topics = get_topics_in_document(lemmas, dictionary, topic_model)
-                    topics.sort(key=lambda x: x[1], reverse=True)
-                    print(sentence["response"], topics[0])
-                    best_topic = topics[0][0]
-                    topic_dictionary[best_topic].append(sentence["response"])
-
-            for topic in topic_dictionary:
-                topic_branch = {"name": f"topic{topic}",
-                                "children": []}
-
-                for response in topic_dictionary[topic]:
-                    topic_branch["children"].append({"name": response,
-                                                    "value": 1})
-
-                main_verb_branch["children"].append(topic_branch)
-
-            main_subject_branch["children"].append(main_verb_branch)
-
-        output["children"].append(main_subject_branch)
-
-        """
-        texts = []
-        for sentence in main_verb_groups[main_verb]:
-            if len(sentence["verbs"]) > 3:
-                texts.append(" ".join(sentence["verbs"][3]))
-            else:
-                texts.append("")
-
-        
-        text_clusters = cluster(texts)
-
-        print("--")
-        print(main_verb)
-
-        clusters_and_sentences = list(zip(text_clusters, main_verb_groups[main_verb]))
-        clusters_and_sentences.sort(key=lambda x:x[0])
-
-        for (c, sentence) in clusters_and_sentences:
-            print(c, sentence["response"])
-        """
+    create_visualization_data(all_sentences, "tree.json")
 
     # This is the output for the Google doc
+    columns = ["response", "verb construction", "main verb", "extracted arg0", "intended arg0", "arg1", "arg2"]
+    print("\t".join(columns))
     for sentence in all_sentences:
         output_list = [sentence["response"], " ".join(sentence["meta"][1:])]  # strip the first "meta" label: main clause
         for verb in sentence["verbs"][3:]:
@@ -212,10 +230,6 @@ def parse_srl_output(srl_output_list, topic_model, dictionary):
                 output_list.append(item)
 
         print("\t".join(output_list))
-
-    # This is the output for the visualization tree.
-    with open("tree.json", "w") as o:
-        json.dump(output, o)
 
     topics = list(topic_model.print_topics())
     with open("topics.json", "w") as o:
