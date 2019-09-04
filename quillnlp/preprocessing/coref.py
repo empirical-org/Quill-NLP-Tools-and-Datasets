cf0baacfb00f687b7497c30f6e023a36867509f8
yvespeirsman <yves@nlp.town>
2019-09-04 11:33:55 +0200

    Updated SRL scripts

---
+++ quillnlp/preprocessing/coref.py
@@ -0,0 +1,32 @@
+from quillnlp.utils import detokenize
+
+from allennlp.predictors.predictor import Predictor
+
+
+coref_predictor = Predictor.from_path("https://s3-us-west-2.amazonaws.com/allennlp/models/coref-model-2018.02.05.tar.gz")
+
+
+def get_coreference_dictionary(sentence: str):
+    """
+    Takes a sentence and returns a dictionary where word indices are mapped to their
+    antecedent token.
+
+    Args:
+        sentence: the input sentence
+
+    Returns: a dictionary with coreference information, e.g. {"13-13": "Schools"}
+    """
+
+    prediction = coref_predictor.predict(document=sentence)
+
+    corefs = {}
+    for cluster in prediction["clusters"]:
+        antecedent_token_start = cluster[0][0]
+        antecedent_token_end = cluster[0][1]
+        antecedent_token = prediction["document"][antecedent_token_start: antecedent_token_end + 1]
+        for coreferent in cluster[1:]:
+            coreferent_token_start = coreferent[0]
+            coreferent_token_end = coreferent[1]
+            coreferent_token_location = str(coreferent_token_start) + "-" + str(coreferent_token_end)
+            corefs[coreferent_token_location] = detokenize(" ".join(antecedent_token))
+    return corefs
--- scripts/perform_srl.py
+++ scripts/perform_srl.py
@@ -1,19 +1,24 @@
 import json
+import click
 
-from quillnlp.srl import perform_srl
-
-
-PROMPT = "The United States is investing in self-driving trucks, so"
-INPUT_FILE = "data/raw/selfdrivingtrucks_so.tsv"
-OUTPUT_FILE = "scripts/data/selfdrivingtrucks_so_srl.json"
+from quillnlp.preprocessing.srl import perform_srl
 
 
-with open(INPUT_FILE) as i:
-    responses = [line.strip() for line in i]
+@click.command()
+@click.argument('input_file')
+@click.argument('output_file')
+@click.option('--prompt', default=None, help='An optional prompt')
+def srl(input_file, output_file, prompt):
+
+    print(prompt)
+    with open(input_file) as i:
+        responses = [line.strip() for line in i]
+
+    results = perform_srl(responses, prompt)
+
+    with open(output_file, "w") as o:
+        json.dump(results, o)
 
 
-results = perform_srl(responses, PROMPT)
-
-
-with open(OUTPUT_FILE, "w") as o:
-    json.dump(results, o)
+if __name__ == "__main__":
+    srl()
