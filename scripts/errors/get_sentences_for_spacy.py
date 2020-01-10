9d7ac7f55e172bb0e71e569cc755a9a28414f48f
yvespeirsman <yves@nlp.town>
2020-01-10 17:16:03 +0100

    Added function for finding adverbs

---
+++ scripts/errors/get_sentences_for_spacy.py
@@ -0,0 +1,53 @@
+import spacy
+import json
+import click
+from functools import partial
+from tqdm import tqdm
+from quillnlp.grammar import corpus
+
+
+error_types = {"VB": corpus.has_infinitive,
+               "NNS": corpus.has_plural_noun,
+               "POS": corpus.has_possessive_noun,
+               "VBP": corpus.has_present_verb_non_third_person,
+               "VBZ": corpus.has_third_person_singular_verb,
+               "ADV": corpus.has_adverb,
+               "its": partial(corpus.contains_token, "its"),
+               "it's": partial(corpus.contains_phrase, ["it", "'s"]),
+               "child": partial(corpus.contains_token, "child"),
+               "children": partial(corpus.contains_token, "children"),
+               "woman": partial(corpus.contains_token, "woman"),
+               "women": partial(corpus.contains_token, "women"),
+               "than": partial(corpus.contains_token, "than"),
+               "then": partial(corpus.contains_token, "then")
+               }
+
+
+@click.command()
+@click.argument('filename')
+@click.argument('number')
+@click.argument('output_file')
+def create_corpus(filename, number, output_file):
+
+    number = int(number)
+    error_corpus = {error: [] for error in error_types}
+    nlp = spacy.load("en")
+    with open(filename) as i:
+        for line in i:
+            line = line.strip()
+            if "=" in line:
+                continue
+
+            doc = nlp(line)
+
+            for error_type in error_types:
+                if len(error_corpus[error_type]) < number:
+                    if error_types[error_type](doc):
+                        error_corpus[error_type].append(line)
+
+    with open(output_file, "w") as o:
+        json.dump(error_corpus, o)
+
+
+if __name__ == "__main__":
+    create_corpus()
