fe43cc9090a95b2ab51a780f5a1f2e6a5f2abfe7
yvespeirsman <yves@nlp.town>
2020-03-24 14:06:07 +0100

    Added grammar tests with Bing

--- tests/test_grammar.py
+++ tests/test_grammar.py
@@ -1,6 +1,6 @@
 import csv
 import requests
-import time
+import ndjson
 
 from private import BING_URL, BING_KEY
 
@@ -140,25 +140,21 @@ def test_yesno():
 
 def test_grammar_quill():
 
-    #checker = SpaCyGrammarChecker("models/grammar20M")
-    checker = BertGrammarChecker("/tmp/model.bin")
+    checker = SpaCyGrammarChecker("models/grammar20M")
+    #checker = BertGrammarChecker("/tmp/model.bin")
 
     # Read error input
-    data = []
-    with open("tests/data/grammar.tsv") as i:
-        reader = csv.reader(i, delimiter="\t")
-        for row in reader:
-            if len(row) > 1:
-                data.append(row)
+    with open("tests/data/grammar_bing.ndjson") as i:
+        data = ndjson.load(i)
 
     results = {}
 
     error_counts = Counter()
     with open("results.tsv", "w") as o:
         for item in data:
-            sentence = item[0]
+            sentence = item["original_sentence"]
             #sentence = correct_sentence_with_bing(sentence)
-            errors = set([error_map.get(e.strip(), e.strip()) for e in item[1:] if len(e.strip()) > 0])
+            errors = set([error_map.get(e.strip(), e.strip()) for e in item["original_errors"] if len(e.strip()) > 0])
             error_counts.update(errors)
 
             found_errors = checker.check(sentence)
@@ -228,37 +224,83 @@ def test_grammar_pairs():
             if len(line) == 3:
                 data.append(line)
 
+
+def evaluate_bing_corrections(data):
+    counter_before = Counter()
+    counter_after = Counter()
+
+    for item in data:
+        counter_before.update(item["original_errors"])
+        counter_after.update(item["errors_after_bing"])
+
+    correction_percentage = []
+    for error in counter_before:
+        correction_percentage.append((1-(counter_after.get(error, 0)/counter_before.get(error)), error))
+
+    correction_percentage.sort(reverse=True)
+    print("Bing corrections")
+    for (p, e) in correction_percentage:
+        print(p, e)
+
+
+def test_bing():
+
+    # Read error input
+    with open("tests/data/grammar_bing.ndjson") as i:
+        data = ndjson.load(i)
+
+    # Evaluate Bing corrections
+    evaluate_bing_corrections(data)
+
+    checker = SpaCyGrammarChecker("models/grammar20M")
+
+    # Evaluate cumulative corrections
+    error_counts = Counter()
     results = {}
-    for (error_type, correct_sentence, incorrect_sentence) in data:
-        if error_type not in results:
-            results[error_type] = {"cc": 0, "ci": 0, "ic": 0, "ii": 0}
+    with open("results_bing_quill.tsv", "w") as o:
+        for item in data:
+            sentence = item["sentence_after_bing"]
+            errors = set([error_map.get(e.strip(), e.strip()) for e in item["original_errors"] if len(e.strip()) > 0])
+            error_counts.update(errors)
 
-        errors = checker.check(correct_sentence)
-        errors = [e for e in errors if e.type.startswith(error_type)]
-        print(correct_sentence)
-        print(errors)
+            # Get the errors our own grammar model finds
+            quill_errors = checker.check(sentence)
+            quill_error_types = set([error_map.get(e[2], e[2]) for e in quill_errors])
 
-        if len(errors) == 0:
-            results[error_type]["cc"] += 1
-        else:
-            results[error_type]["ci"] += 1
+            # Combine these with the errors Bing finds
+            bing_errors = set(item["original_errors"]) - set(item["errors_after_bing"])
+            bing_error_types = set([error_map.get(e, e) for e in bing_errors])
 
-        errors = checker.check(incorrect_sentence)
-        errors = [e for e in errors if e.type.startswith(error_type)]
-        print(incorrect_sentence)
-        print(errors)
+            found_error_types = quill_error_types | bing_error_types
 
-        if len(errors) == 0:
-            results[error_type]["ic"] += 1
-        else:
-            results[error_type]["ii"] += 1
+            for error_type in errors:
+                if error_type not in results:
+                    results[error_type] = {"tp": 0, "fp": 0, "fn": 0}
 
-    for error_type in results:
-        correct = results[error_type]["cc"] + results[error_type]["ii"]
-        incorrect = results[error_type]["ic"] + results[error_type]["ci"]
+                if error_type in found_error_types:
+                    results[error_type]["tp"] += 1
+                else:
+                    results[error_type]["fn"] += 1
 
-        precision = results[error_type]["cc"]/ (results[error_type]["cc"] + results[error_type]["ic"])
-        recall = results[error_type]["cc"] / (results[error_type]["cc"] + results[error_type]["ci"])
-        fscore = 2*precision*recall/(precision+recall)
+            for found_error in found_error_types:
+                if found_error not in errors:
+                    if found_error not in results:
+                        results[found_error] = {"tp": 0, "fp": 0, "fn": 0}
 
-        print(error_type, precision, recall, fscore)
+                    results[found_error]["fp"] += 1
+
+            o.write("\t".join([sentence, ",".join(errors), ",".join(found_error_types)]) + "\n")
+            #time.sleep(2)
+
+    error_types = sorted(list(results.keys()))
+    for error_type in error_types:
+        tp = results[error_type]["tp"]
+        fp = results[error_type]["fp"]
+        fn = results[error_type]["fn"]
+        support = error_counts[error_type]
+
+        precision = tp/(tp+fp) if (tp+fp) > 0 else 0
+        recall = tp/(tp+fn) if (tp+fn) > 0 else 0
+        fscore = 2*precision*recall/(precision+recall) if precision+recall > 0 else 0
+
+        print(f"{error_type}\t{precision}\t{recall}\t{fscore}\t{support}")
