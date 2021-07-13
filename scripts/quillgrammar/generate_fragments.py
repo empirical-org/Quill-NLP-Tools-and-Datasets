db8d4f568163ae3c691e74be3bcf96e7c916c7ec
yvespeirsman <yvespeirsman@gmail.com>
2021-07-13 14:49:03 +0200

    Added updated fragment generation script

--- scripts/quillgrammar/generate_fragments.py
+++ scripts/quillgrammar/generate_fragments.py
@@ -2,51 +2,145 @@ import os
 import csv
 import random
 from tqdm import tqdm
+from collections import Counter
 
 import spacy
 from spacy.tokens import DocBin
 
-from quillnlp.grammar.fragments import FragmentWithoutVerbGenerator, FragmentWithoutSubjectGenerator
+from quillnlp.grammar.fragments import FragmentWithoutVerbGenerator, FragmentWithoutSubjectGenerator, \
+    MissingObjectFragmentGenerator, prepositionalPhraseFragmentGenerator, adverbialClauseFragmentGenerator, \
+    relativeClauseFragmentGenerator
 from quillnlp.grammar.myspacy import nlp
+from quillnlp.corpora.notw import read_sentences
 
-f = '/home/yves/projects/grammar-api/quillgrammar/data/new_comprehension.csv'
-output_path = '/tmp/fragments/'
+#f = '/home/yves/projects/grammar-api/quillgrammar/data/new_comprehension.csv'
+f = '/home/yves/projects/grammar-api/new_comprehension.tsv'
+output_path = '.'
 
-generator1 = FragmentWithoutSubjectGenerator()
-generator2 = FragmentWithoutVerbGenerator()
+NO_FRAGMENT = 'no_fragment'
+MISSING_SUBJECT_FRAGMENT = 'fragment_no_subject'
+MISSING_VERB_FRAGMENT = 'fragment_no_verb'
+MISSING_OBJECT_FRAGMENT = 'fragment_missing_object'
+PP_FRAGMENT = 'fragment_prepositional_phrase'
+ADV_CL_FRAGMENT = 'fragment_adverbial_clause'
+REL_CL_FRAGMENT = 'fragment_relative_clause'
 
-data = []
 
-with open(f) as i:
-    reader = csv.reader(i, delimiter=',')
-    for line in tqdm(reader):
-        sentence, prompt, _, _, _ = line
+def create_instance(sentence, prompt):
+    generator_no_subj = FragmentWithoutSubjectGenerator()
+    generator_no_verb = FragmentWithoutVerbGenerator()
+    generator_no_obj = MissingObjectFragmentGenerator()
+    generator_pp = prepositionalPhraseFragmentGenerator
+    generator_advcl = adverbialClauseFragmentGenerator
+    generator_relcl = relativeClauseFragmentGenerator
 
-        random_number = random.random()
-        if random_number < 0.25:
-            generator = generator1
-            doc = nlp(sentence)
-            sentence, _, _ = generator.generate_from_doc(doc, prompt)
-            label = 'fragment'
-        elif random_number < 0.5:
-            generator = generator2
-            doc = nlp(sentence)
-            sentence, _, _ = generator.generate_from_doc(doc, prompt)
-            label = 'fragment'
-        else:
-            label = 'no_fragment'
+    candidate_fragments = []
+    doc = nlp(sentence)
+    for label, generator in [(MISSING_SUBJECT_FRAGMENT, generator_no_subj),
+                             (MISSING_OBJECT_FRAGMENT, generator_no_obj),
+                             (MISSING_VERB_FRAGMENT, generator_no_verb),
+                             (PP_FRAGMENT, generator_pp),
+                             (ADV_CL_FRAGMENT, generator_advcl),
+                             (REL_CL_FRAGMENT, generator_relcl)]:
+        fragment, _, relevant = generator.generate_from_doc(doc, prompt)
+        if relevant:
+            candidate_fragments.append((label, fragment))
 
-        data.append((sentence, label))
+    random_number = random.random()
+
+    if random_number < 0.5:
+        label, sentence = random.choice(candidate_fragments)
+    else:
+        label = NO_FRAGMENT
+
+    return sentence, label
+
+
+def read_csv_input(f):
+
+    data = []
+    instance_set = set()
+
+    print('Creating fragments from Quill data')
+    with open(f) as i:
+        reader = csv.reader(i, delimiter=',')
+        for line in tqdm(reader):
+            sentence, prompt, _, _, _ = line
+
+            instance, label = create_instance(sentence, prompt)
+
+            if instance not in instance_set:
+                data.append((instance, label))
+                instance_set.add(instance)
+
+    print('Fetching NOTW data')
+    with open('data/raw/notw_sentences.txt') as i:
+        notw_sentences = [line.strip() for line in i]
+
+    print('Creating fragments from NOTW data')
+    for sentence in tqdm(notw_sentences):
+        instance, label = create_instance(sentence, prompt)
+
+        if instance not in instance_set:
+            data.append((instance, label))
+            instance_set.add(instance)
+
+    return data
+
+
+def read_grammar_model_output(f):
+    data = []
+
+    instance_set = set()
+    print('Creating fragments from Quill data')
+    with open(f) as i:
+        reader = csv.reader(i, delimiter='\t')
+        for line in tqdm(reader):
+            sentence, prompt, error, _, _ = line
+            if error == '':
+                instance, label = create_instance(sentence, prompt)
+                if instance not in instance_set:
+                    data.append((instance, label))
+                    instance_set.add(instance)
+
+    print('Fetching NOTW data')
+    with open('data/raw/notw_sentences.txt') as i:
+        notw_sentences = [line.strip() for line in i]
+
+    print('Creating fragments from NOTW data')
+    for sentence in tqdm(notw_sentences[:200000]):
+        instance, label = create_instance(sentence, '')
+
+        if instance not in instance_set:
+            data.append((instance, label))
+            instance_set.add(instance)
+
+    print('Instances:', len(data))
+    labels = Counter([d[1] for d in data])
+    print(labels)
+
+    return data
+
+
+data = read_grammar_model_output(f)
+
 
 def write_output(data, output_path):
     db = DocBin()
     nlp = spacy.blank('en')
     for sentence, label in tqdm(data):
         doc = nlp.make_doc(sentence)
-        if label == 'fragment':
-            doc.cats = {'fragment': 1.0, 'no_fragment': 0}
-        else:
-            doc.cats = {'fragment': 0, 'no_fragment': 1.0}
+        doc.cats = {
+            NO_FRAGMENT: 0,
+            MISSING_VERB_FRAGMENT: 0,
+            MISSING_SUBJECT_FRAGMENT: 0,
+            MISSING_OBJECT_FRAGMENT: 0,
+            PP_FRAGMENT: 0,
+            ADV_CL_FRAGMENT: 0,
+            REL_CL_FRAGMENT: 0
+        }
+
+        doc.cats[label] = 1.0
         db.add(doc)
     db.to_disk(output_path)
 
