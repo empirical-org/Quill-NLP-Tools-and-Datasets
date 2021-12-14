4bfa3c9e258f6d1f538dd562c5ebb0e1db43b205
yvespeirsman <yvespeirsman@gmail.com>
2021-12-14 15:35:51 +0100

    Updated grammar pipeline part 2

---
+++ scripts/quillgrammar/prepare_spaCy_grammar_corpus.py
@@ -0,0 +1,85 @@
+import os
+import random
+import click
+from tqdm import tqdm
+from grammar_corpus import read_grammar_data
+
+import spacy
+from spacy.tokens import DocBin, Span
+
+from quillnlp.grammar.myspacy import nlp
+from quillnlp.corpora.notw import read_sentences
+
+def find_index_from_offsets(doc, start, end):
+
+    first_token_start = None
+    next_token_start = None
+
+    for token in doc:
+        if token.idx >= start and first_token_start is None:
+            first_token_start = token.i
+        if token.idx > end and next_token_start is None:
+            next_token_start = token.i
+
+    if next_token_start is None:
+        next_token_start = len(doc)
+
+    return first_token_start, next_token_start
+
+
+
+def write_output(data, output_path):
+    db = DocBin()
+    nlp = spacy.blank('en')
+
+    random.shuffle(data)
+    for sentence, label in tqdm(data):
+        doc = nlp.make_doc(sentence)
+
+        if isinstance(label, dict):
+            entities = []
+            start_idx_returned_none = False
+            for ent in label['entities']:
+                start_idx, end_idx = find_index_from_offsets(doc, ent[0], ent[1])
+                if start_idx and end_idx:
+                    entities.append(Span(doc, start_idx, end_idx, ent[2]))
+                else:
+                    start_idx_returned_none = True
+            try:
+                if not start_idx_returned_none:
+                    doc.set_ents(entities)
+                    db.add(doc)
+            except:
+                continue
+        else:
+            raise ValueError('Unknown label type for', label)
+
+
+    db.to_disk(output_path)
+
+
+@click.command()
+@click.argument('output_path')
+def run(output_path):
+
+    # Read grammar data    
+    grammar_train, grammar_dev, grammar_test = read_grammar_data()
+
+    train_files_path = os.path.join(output_path, 'train')
+
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+
+    if not os.path.exists(train_files_path):
+        os.mkdir(train_files_path)    
+
+    write_output(grammar_test, os.path.join(output_path, 'test.spacy'))
+    write_output(grammar_dev, os.path.join(output_path, 'dev.spacy'))
+
+    chunk_size = 1000000
+    for i in range(0, len(grammar_train), chunk_size):
+        write_output(grammar_train[i:i+chunk_size], os.path.join(train_files_path, f"train{int(i/chunk_size)}.spacy"))
+
+
+if __name__ == '__main__':
+    run()
